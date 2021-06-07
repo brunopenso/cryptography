e7fbd2844d082945771c4c869c0806a8bfb0e9d6
Bruno Penso <bruno.penso@gmail.com>
2021-06-07 17:51:01 -0300

    decrypt method

--- algo1.js
+++ algo1.js
@@ -1,8 +1,6 @@
 const alphabet = require('./alphabet')
 
 function encrypt(givenText) {
-    console.log(`Encrypting this text ${givenText}`)
-
     let textToEncrypt = ''
 
     const textToWork = givenText.toLocaleLowerCase()
@@ -12,8 +10,6 @@ function encrypt(givenText) {
             textToEncrypt += char
         }
     }
-
-    console.log(`The text that will be encrypt is ${textToEncrypt}`)
 
     const textToEncryptArray = textToEncrypt.split('')
     let railFenceLine1 = ''
@@ -26,15 +22,45 @@ function encrypt(givenText) {
             railFenceLine2 += char
         }
     }
-
-    console.log(`Line 1: ${railFenceLine1}`)
-    console.log(`Line 2: ${railFenceLine2}`)
+    console.log('\tRail fence line 1', railFenceLine1)
+    console.log('\tRail fence line 2', railFenceLine2)
 
     return railFenceLine1+railFenceLine2
 }
 
+function decrypt(givenText) {
+    const line1 = givenText.substring(0, (givenText.length / 2)+1)
+    const line2 = givenText.substring((givenText.length / 2)+1, givenText.length)
+    
+    let text = ''
+    let currentLength = 0
+    while(true) {
+        const charLine1 = line1.charAt(currentLength)
+        const charLine2 = line2.charAt(currentLength)
+        if (charLine1 !== '') {
+            text += charLine1
+        }
+        if (charLine2 !== '') {
+            text += charLine2
+        }
+        if (charLine1 === '' && charLine2 === '') {
+            break
+        } else {
+            currentLength++
+        }
+    }
+
+    return text
+}
+
 const myText = 'Thy secret is thy prisoner: if thou let it go, thou art a prisoner to it'
+
+console.log('Text to encrypt:', myText)
 
 const result = encrypt(myText)
 
-console.log(`Encrypted text: ${result}`)
+console.log('Encrypted text:', result)
+
+const result1 = decrypt(result)
+
+console.log('Decrypted message:', result1)
