efe6b6c1ab3c1108947b477a67927f3d28a860be
Bruno Penso <bruno.penso@gmail.com>
2021-06-07 18:38:07 -0300

    add shiftAlphabet

--- src/alphabet.js
+++ src/alphabet.js
@@ -5,8 +5,13 @@ function getAlphabet(c1 = 'a', c2 = 'z') {
     return (a.slice(a.indexOf(c1), a.indexOf(c2) + 1)); 
 }
 
+function shiftAlphabet(qtdy) {
+    const part1 = alphabetString.substr(0, qtdy)
+    const part2 = alphabetString.substr(qtdy, alphabetString.length)
+    return part2+part1
+}
 module.exports = {
     default: getAlphabet(),
     getAlphabet,
-    alphabetString
+    shiftAlphabet,
 }
